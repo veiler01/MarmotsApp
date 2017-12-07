c16f371e4e1ab58aa6238e0c9a57b56f79ca8c0b
Daniele Migliaccio <danielemigliaccio@gmail.com>
2017-12-07 16:05:33 +0100

    Initial Commit

---
+++ MarmotsApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MarmotsApp
+//
+//  Created by Daniele Migliaccio on 07/12/2017.
+//  Copyright © 2017 Daniele Migliaccio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
